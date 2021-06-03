c23726593afe4ad6693bc9e93bf7f8b5b60bda5c
Deepyaman Das <54535773+deepyaman-das@users.noreply.github.com>
2021-06-04 00:32:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -75,8 +75,8 @@ $(document).ready(function(){
     form.addEventListener('submit', e => {
         e.preventDefault()
         fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-            .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+            .then(response => console.log("Thanks for Contacting us..! We Will Contact You Soon...",response))
             .catch(error => console.error('Error!', error.message))
-        document.getElementById("google-sheet").reset();
+        document.getElementById("myForm").reset();
     });
 });
